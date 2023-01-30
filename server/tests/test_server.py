71cbc53e574f51e13a4d2e8c8bd2c6bb915ffa26
sreeharin <sreeharinagesh@gmail.com>
2023-01-30 15:40:01 +0530

    More tests for server

--- server/tests/test_server.py
+++ server/tests/test_server.py
@@ -1,32 +1,51 @@
-import sys
 from twisted.trial import unittest
 from twisted.test import proto_helpers
 from json import dumps
+import sys
 
 sys.path.append('../')
-from ttt_server import TicTacToeProtocol, TicTacToeFactory
+from ttt_server import TicTacToeFactory
 
 class TestServer(unittest.TestCase):
     def setUp(self):
-        # self.protocol = TicTacToeProtocol() 
         self.factory = TicTacToeFactory()
         self.protocol = self.factory.buildProtocol(['127.0.0.1', 0])
         self.tr = proto_helpers.StringTransport()
         self.protocol.makeConnection(self.tr)
 
+    def test_invalid_input(self):
+        self.protocol.lineReceived(b'test')
+        res = {"response": "invalid input"}
+        self.assertEqual(self.tr.value(), dumps(res).encode('utf-8') + b'\r\n')
+
+    def test_invalid_game_code(self):
+        join = {"action": "JOIN", "data": {"game_code": "test"}}
+        self.protocol.lineReceived(dumps(join).encode('utf-8'))
+        res = {"response": "invalid game_code"}
+        self.assertEqual(self.tr.value(), dumps(res).encode('utf-8') + b'\r\n')
+
     def test_host_game(self):
-        tmp = {
-            "action": "HOST"
-        }
-        res = {
-            "response": "room created"
-        }
+        tmp = {"action": "HOST", "data": None}
+        res = {"response": "room created"}
+        self.assertEqual(len(self.factory.game_rooms), 0)
         self.protocol.lineReceived(dumps(tmp).encode('utf-8'))
         self.assertEqual(self.tr.value(), dumps(res).encode('utf-8') + b'\r\n')
         self.assertEqual(len(self.factory.game_rooms), 1)
 
     def test_join_game(self):
-        raise Exception("Not implemented")
+        host = {"action": "HOST", "data": None}
+        self.protocol.lineReceived(dumps(host).encode('utf-8'))
+        game_code = list(self.factory.game_rooms.keys())[0]
+        join = {"action": "JOIN", "data": {"game_code": game_code}}
+        self.tr.clear()
+        self.protocol.lineReceived(dumps(join).encode('utf-8'))
+        res = {"response": "joined room"}
+        self.assertEqual(self.tr.value(), dumps(res).encode('utf-8') + b'\r\n')
+        self.assertEqual(len(self.factory.game_rooms[game_code].players), 2)
 
     def test_cancel_game(self):
         raise Exception("Not implemented")
+
+    def test_eval(self):
+        raise Exception("Not implemented")
+
--- server/ttt_server.py
+++ server/ttt_server.py
@@ -12,50 +12,64 @@ logging.basicConfig(
 )
 
 class TicTacToeProtocol(basic.LineReceiver):
-    def connectionMade(self):
-        addr = self.transport.getPeer()
-        logging.info('A new connection is established: %s' % (addr.host))
+    # def connectionMade(self):
+    #     addr = self.transport.getPeer()
+    #     logging.info('A new connection is established: %s' % (addr.host))
 
     def lineReceived(self, line):
-        res = None
         try:
             line = line.decode('utf-8')
             json_data = json.loads(line)
             self.handle_line(json_data)
         except json.decoder.JSONDecodeError:
-            res = {
-                'response': 'Invalid input'
-            }
-            self.transport.write(json.dumps(res).encode('utf-8'))
+            self.write_response('invalid input')
 
     def handle_line(self, json_data):
-        action = json_data['action']
-        # data = json_data['data']
+        action = json_data.get('action', None)
+        data = json_data.get('data', None)
 
         if action == 'HOST':
+            logging.info(
+                'New game room created by : %s' % self.transport.getPeer()
+            )
             gc = game_code()
-            self.factory.game_rooms.append(Room(
+            self.factory.game_rooms[gc] = Room(
                 game_code = gc, players = [self.transport.getPeer()]
-                ))
+            )
             self.write_response('room created') 
+
         elif action == 'JOIN':
-            pass
-        elif action == 'EVAL':
+            if data == None:
+                self.write_response('invalid data')
+                return
+
+            gc = data.get('game_code', None) 
+            if gc == None:
+                self.write_response('game_code not found')
+                return
+
+            if gc not in self.factory.game_rooms:
+                self.write_response('invalid game_code')
+                return
+
+            self.factory.game_rooms[gc].players.append(
+                self.transport.getPeer()
+            )
+            self.write_response('joined room')
+        elif action == 'CANCEL':
             pass
         else:
             pass
 
     def write_response(self, msg: str) -> None:
-        res = {
-            'response': msg
-        }
+        res = {'response': msg}
         self.sendLine(json.dumps(res).encode('utf-8'))
 
 class TicTacToeFactory(protocol.ServerFactory):
     protocol = TicTacToeProtocol
 
     def __init__(self):
-        self.game_rooms = []
+        self.game_rooms = {} 
 
 if __name__ == '__main__':
     logging.info('Starting Tic Tac Toe server')
