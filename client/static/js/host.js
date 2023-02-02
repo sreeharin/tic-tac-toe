7ebd2078f2754e6a0a754672cc28e8488ce31cee
sreeharin <sreeharinagesh@gmail.com>
2023-02-02 13:04:35 +0530

    Tidy up

--- client/static/js/host.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#copy-code").on("click", function(){
-        var game_code = $("#game-code").val();
-        navigator.clipboard.writeText(game_code);
-    });
-});
